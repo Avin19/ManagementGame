f223c3cbc7ab300f3e3d8bdd0c34c8419dc4a831
Avinash Yadav <avinashy1993@gmail.com>
2025-06-28 14:20:28 +0530

    Trash Counter

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -1,11 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
+    public enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CamerForword,
+        CamerForwordInverted,
+    }
+
+    [SerializeField] private Mode mode;
+
     void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform);
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 LookDir = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + LookDir);
+                break;
+            case Mode.CamerForword:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CamerForwordInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
     }
 }
